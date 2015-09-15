4897ad76cbc6fc92e77211ffb875c61589a0a290
Lindsay Culver & Yelena Nedelko <student@epicodus.com>
2015-09-15 14:24:11 -0700

    Add initial web view.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
-function Ticket(movieName, movieTime, userAge) {
+function Ticket(movieName, movieTime, userAge, newRelease) {
   this.movieName = movieName;
   this.movieTime = movieTime;
   this.userAge = userAge;
+  this.newRelease = newRelease;
+  this.ticketPrice = 10;
+}
+
+Ticket.prototype.ticketPrice = function() {
+  xxx
+  xxx
+  xxx
+
+  return this.ticketPrice;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,9 @@
 describe('Ticket', function() {
   it("create a new ticket with the given specification", function() {
-    var testTicket = new Ticket("Cool Movie", "early", 30);
+    var testTicket = new Ticket("Cool Movie", "early", 30, true);
     expect(testTicket.movieName).to.equal("Cool Movie");
     expect(testTicket.movieTime).to.equal("early");
     expect(testTicket.userAge).to.equal(30);
+    expect(testTicket.newRelease).to.equal(true);
   });
 });
