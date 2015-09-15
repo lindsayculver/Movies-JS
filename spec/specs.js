778307956bc22e249e6bd94d9178dcc5f539ea22
Lindsay Culver & Yelena Nedelko <student@epicodus.com>
2015-09-15 16:31:45 -0700

    Add spec to test calculation for ticket price given user inputs.

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,14 @@
 describe('Ticket', function() {
   it("create a new ticket with the given specification", function() {
-    var testTicket = new Ticket("Cool Movie", "early", 30, true);
+    var testTicket = new Ticket("Cool Movie", "early", 30, "Yes");
     expect(testTicket.movieName).to.equal("Cool Movie");
     expect(testTicket.movieTime).to.equal("early");
     expect(testTicket.userAge).to.equal(30);
-    expect(testTicket.newRelease).to.equal(true);
+    expect(testTicket.newRelease).to.equal("Yes");
+  });
+
+  it("calculates ticket price for user inputs", function() {
+    var testTicket = new Ticket("Cool Movie", "matinee", 30, "No");
+    expect(testTicket.ticketPrice()).to.equal(5);
   });
 });
